90325f2ca50c2597ee199c08c68a6fdb128673dd
rodeox1980 <rodeox.1980@gmail.com>
2022-10-30 00:28:41 +0900

    feat(無し): 🎸 新規作成

---
+++ src/main/java/com/example/bookret/bookret/BookretApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookret.bookret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookretApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookretApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookret/bookret/BookretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookret.bookret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookretApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
